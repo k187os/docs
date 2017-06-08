1675314019bb91fec03e9278a3409edf8d88e372
k187os <k187os@gmail.com>
2017-06-08 01:43:22 +0100

    first git

--- populate.py
+++ populate.py
@@ -17,7 +17,10 @@ for line in f:
     drug_forme = line[155:176]
     drug_obs = line[266:]
     if drug_nom != "":
-        c.execute('INSERT INTO clinic_drug (drug_nom, drug_dci, drug_forme, drug_dose, drug_cnas , drug_obs  )VALUES (?, ? , ? , ? , ? , ?)', (drug_nom.strip(), drug_dci.strip(), drug_forme.strip(), drug_dose.strip(), drug_cnas.strip(), drug_obs.strip()) )
+        c.execute('INSERT INTO clinic_drug (drug_nom, drug_dci, drug_forme, drug_dose, drug_cnas , drug_obs  )VALUES '
+                  '(?, ? , ? , ? , ? , ?)', (drug_nom.strip(), drug_dci.strip(), drug_forme.strip(), drug_dose.strip(
+
+        ), drug_cnas.strip(), drug_obs.strip()) )
 
 conn.commit()
 f.close()
